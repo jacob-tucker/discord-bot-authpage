aad9533f71b5069ce024242b43b7b8243cf2cd77
Jacob Tucker <tsnakejake@aol.com>
2021-12-05 15:58:38 -0600

    just blocto

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import * as fcl from '@onflow/fcl';
 
 fcl.config()
   .put('accessNode.api', 'https://access-testnet.onflow.org')
-  .put('discovery.wallet', 'https://fcl-discovery.onflow.org/testnet/authn');
+  .put('discovery.wallet', 'https://flow-wallet-testnet.blocto.app/api/flow/authn');
 
 function App(props) {
   const [user, setUser] = useState();
