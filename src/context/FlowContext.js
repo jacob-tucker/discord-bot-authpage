57a67ac9af15a0757b576a901e31e7cb53b5695b
Jacob Tucker <tsnakejake@aol.com>
2022-01-12 10:53:13 -0600

    fix script

--- src/context/FlowContext.js
+++ src/context/FlowContext.js
@@ -41,7 +41,7 @@ function Provider(props) {
             import EmeraldIdentity from 0x4e190c2eb6d78faa
 
             pub fun main(account: Address): String? {    
-                return EmeraldIdentity.getDiscordFromAccount(account: account)?
+                return EmeraldIdentity.getDiscordFromAccount(account: account)
             }
             `,
             fcl.args([
