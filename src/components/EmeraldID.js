435cf80409fea010855933c48104b29e1b79cd76
Jacob Tucker <tsnakejake@aol.com>
2022-01-12 11:53:01 -0600

    color

--- src/components/EmeraldID.js
+++ src/components/EmeraldID.js
@@ -6,6 +6,7 @@ import FlowContext from '../context/FlowContext';
 function EmeraldID(props) {
     const flow = useContext(FlowContext);
     const [message, setMessage] = useState(null);
+    const [status, setStatus] = useState("blue");
 
     useEffect(() => {
         fcl.currentUser().subscribe(flow.setUser);
@@ -21,7 +22,8 @@ function EmeraldID(props) {
                 if (!setup) {
                     createEmeraldID();
                 } else {
-                    setMessage("You have already set up your EmeraldID :)")
+                    setMessage("You have already set up your EmeraldID :)");
+                    setStatus("green");
                 }
             }
 
@@ -31,18 +33,21 @@ function EmeraldID(props) {
 
     const createEmeraldID = async () => {
         setMessage('Setting up your EmeraldID. Please wait ~30 seconds.');
+        setStatus("blue");
         let response = await flow.createEmeraldID();
         if (response === 'Success') {
             setMessage('You have set up your EmeraldID.');
+            setStatus("green");
         } else {
             setMessage(response);
+            setStatus("red");
         }
     }
 
     return (
         <div className="App">
             {flow.user && flow.user.loggedIn && message 
-                ? <h1>{message}</h1>
+                ? <h1 className={status}>{message}</h1>
                 : null}
             <button className="button-9" onClick={() => flow.authentication()}>{flow.user && !flow.user.loggedIn ? "Log in with Blocto" : "Log out"}</button>
         </div>
